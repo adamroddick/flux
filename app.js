e61d2de56c5ae5c069dd5cc6f1d329d5d9269e0d
Adam Roddick <adam@Adams-MacBook-Pro-2.local>
2018-03-25 00:09:27 +1100

    Added Jquery and Bootstrap. Updated index.html to use the new CSS and JS.

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ var app = express();
 
 app.use(morgan('tiny'));
 
+app.use(express.static(path.join(__dirname, '/public/')));
+
 app.get('/', function(req, res) {
-    res.sendfile(path.join(__dirname, 'views/index.html'));
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(3000, function() {
