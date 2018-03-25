3ee020a6a9a71febc2293a6c4bccee0ef2d8b72b
Adam Roddick <adam@Adams-MacBook-Pro-2.local>
2018-03-25 19:21:05 +1100

    Added an Express Router to app.js and cleaned up the index.ejs for arrays in the menus.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+const xRouter = express.Router();
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -16,6 +18,18 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+xRouter.route('/')
+  .get((req, res) => {
+    res.send('Base tabs');
+  });
+
+xRouter.route('/economy')
+  .get((req, res) => {
+    res.send('Economy tab');
+  });
+
+app.use('/tab', xRouter);
 
 app.get('/', (req, res) => {
   // res.sendFile(path.join(__dirname, 'views/index.html'));
@@ -31,9 +45,16 @@ app.get('/', (req, res) => {
       { label: 'Economy', link: '/tab/economy' },
       { label: 'Research', link: '/tab/research' },
       { label: 'Military', link: '/tab/military' },
+      { label: 'Team', link: '/tab/team' },
       { label: 'History', link: '/tab/history' },
     ],
-    menuDropdown: ['dd1', 'dd2', 'dd3'],
+    menuDropdown1: [
+      { label: 'Team', link: '/tab/team' },
+    ],
+    menuDropdown2: [
+      { label: 'Patch Notes', link: '/tab/patchnotes' },
+      { label: 'About', link: '/tab/about' },
+    ],
   });
 });
 
